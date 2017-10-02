e7928571f12de1339d798fca25195a3800902770
H4isan <harlengiraldo@gmail.com>
2017-10-02 10:24:02 -0500

    s

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const api = require('./server/routes/api');
 const app = express();
+
 app.use(express.static(path.join(__dirname,'dist')));
-app.get('*',(req, res)=>{
+app.opts(/\.*/,(req, res)=>{
     res.sendFile(path.join(__dirname,'dist/index.html'));
 });
 const port = process.env.PORT || '3001';
