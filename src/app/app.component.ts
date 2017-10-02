768f07140555bc8591dd6252dbcb61eedb5364e8
H4isan <harlengiraldo@gmail.com>
2017-10-02 15:10:48 -0500

    hola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
-import {DomSanitizer} from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
 declare var MediaRecorder: any;
 
 @Component({
@@ -13,14 +13,53 @@ export class AppComponent implements OnInit {
   buffer = [];
   theRecorder;
   theStream;
-  constructor(private sanitizer:  DomSanitizer, private element: ElementRef) {
+  constructor(private sanitizer: DomSanitizer, private element: ElementRef) {
   }
 
   ngOnInit() {
-}
+  }
 
 
+  showCamMobile() {
+    const nav = <any>navigator;
+    if (nav.mediaDevices === undefined) {
+      nav.mediaDevices = {};
+    }
+    if (nav.mediaDevices.getUserMedia === undefined) {
+      nav.mediaDevices.getUserMedia = (constraints) => {
+        // First get ahold of the legacy getUserMedia, if present
+        const getUserMedia = nav.webkitGetUserMedia || nav.mozGetUserMedia;
 
+        // Some browsers just don't implement it - return a rejected promise with an error
+        // to keep a consistent interface
+        if (!getUserMedia) {
+          return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+        }
+
+        // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+        return new Promise(function (resolve, reject) {
+          getUserMedia.call(navigator, constraints, resolve, reject);
+        });
+      };
+    }
+    navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+      .then((stream) => {
+        const video = this.element.nativeElement.querySelector('video');
+        // Older browsers may not have srcObject
+        if ('srcObject' in video) {
+          video.srcObject = stream;
+        } else {
+          // Avoid using this in new browsers, as it is going away.
+          video.src = window.URL.createObjectURL(stream);
+        }
+        video.onloadedmetadata = function(e) {
+          video.play();
+        };
+      })
+      .catch((err) => {
+        console.log(err.name + ': ' + err.message);
+      });
+  }
 
   public showCam() {
     const nav = <any>navigator;
@@ -29,16 +68,16 @@ export class AppComponent implements OnInit {
       nav.mozGetUserMedia ||
       nav.msGetUserMedia);
     const promise = new Promise<string>((resolve, reject) => {
-          nav.mediaDevices.getUserMedia({video: { facingMode: 'user' }, audio: true}, (stream) => {
-              resolve(stream);
-          }, (err) => reject(err));
-      }).then((mediaStream) => {
-        const video = this.element.nativeElement.querySelector('video');
-        video.srcObject = mediaStream;
-        video.onloadedmetadata = function(e) {
-          video.play();
-        };
-      });
+      nav.mediaDevices.getUserMedia({ video: { facingMode: 'user' }, audio: true }, (stream) => {
+        resolve(stream);
+      }, (err) => reject(err));
+    }).then((mediaStream) => {
+      const video = this.element.nativeElement.querySelector('video');
+      video.srcObject = mediaStream;
+      video.onloadedmetadata = function (e) {
+        video.play();
+      };
+    });
 
   }
   gotMedia(stream) {
@@ -47,9 +86,9 @@ export class AppComponent implements OnInit {
     video.srcObject = (stream);
     try {
       video.play();
-      this.theRecorder = new MediaRecorder(stream, {mimeType : 'video/webm'});
+      this.theRecorder = new MediaRecorder(stream, { mimeType: 'video/webm' });
       this.theRecorder.ondataavailable =
-          (event) => { this.buffer.push(event.data); };
+        (event) => { this.buffer.push(event.data); };
       this.theRecorder.start(100);
     } catch (e) {
       console.error('Exception while creating MediaRecorder: ' + e);
@@ -60,8 +99,8 @@ export class AppComponent implements OnInit {
   download() {
     this.theRecorder.stop();
     this.theStream.getTracks().forEach(track => { track.stop(); });
-    const blob = new Blob(this.buffer, {type: 'video/webm'});
-    const url =  URL.createObjectURL(blob);
+    const blob = new Blob(this.buffer, { type: 'video/webm' });
+    const url = URL.createObjectURL(blob);
     const a: any = document.createElement('a');
     document.body.appendChild(a);
     a.style = 'display: none';
@@ -71,7 +110,7 @@ export class AppComponent implements OnInit {
     // setTimeout() here is needed for Firefox.
     this.playdeoVideo();
 
-      // URL.revokeObjectURL(url);
+    // URL.revokeObjectURL(url);
 
   }
   playdeoVideo() {
